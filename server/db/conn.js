63398715c691448080a63341011caf766dc7e9ef
Daniel Deverell <daniel.deverell@gmail.com>
2021-05-05 21:58:03 -0400

    env for db

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 const conn = new Sequelize(
-  "postgres://postgres:mysecretpassword@localhost:5432/acme_db"
+  process.env.DATABASE_URL ||
+    "postgres://postgres:mysecretpassword@localhost:5432/acme_db"
 );
 
 module.exports = conn;
